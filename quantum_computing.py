d5ffcb103a4bdab6880b86fd90af7f79d3afefc8
Cbaird26 <cbaird26@gmail.com>
2024-05-30 13:57:50 -0400

    Update quantum_computing.py

--- quantum_computing.py
+++ quantum_computing.py
@@ -1,13 +1,14 @@
-from qiskit import QuantumCircuit, transpile, execute, Aer
-from qiskit.providers.ibmq import IBMQ
+# quantum_computing.py
+from qiskit import QuantumCircuit, transpile, IBMQ, execute, Aer
 
 def initialize_ibmq():
-    IBMQ.save_account('526944faf27aeedd952653ff44c8f585f40beb9812cfaca7d4d415ebd4ead190e32ee32ef158ce92c18247d669d2d335bce731f934973d3206b12cf74d91ed2d')
+    IBMQ.save_account('526944faf27aeedd952653ff44c8f585f40beb9812cfaca7d4d415ebd4ead190e32ee32ef158ce92c18247d669d2d335bce731f934973d3206b12cf74d91ed2d', overwrite=True)
     provider = IBMQ.load_account()
     return provider
 
 def create_quantum_circuit():
-    qc = QuantumCircuit(2)
+    qc = QuantumCircuit(2, 2)
     qc.h(0)
     qc.cx(0, 1)
+    qc.measure([0, 1], [0, 1])
     return qc
